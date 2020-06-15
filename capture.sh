358c6e935f0292acf6067b3880534f7c2bb21bfb
Tim Squires <tim@crystalmark.co.uk>
2020-06-15 16:14:07 +0100

    copy mail on error

--- capture.sh
+++ capture.sh
@@ -8,7 +8,7 @@ sleep 2
 i=0
 until python3 capture.py $1 $2 $3 2>&1 >> capture.out
 do
-	scp capture.out *.log tim@crystalmark.co.uk:/var/www/crystalmark.co.uk/boatcam/
+	scp capture.out *.log /var/mail/pi tim@crystalmark.co.uk:/var/www/crystalmark.co.uk/boatcam/
 	((i=i+1))
 	[[ i -eq 10 ]] && echo "Failed!" && exit 1
 	sleep 30
