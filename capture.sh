2569856d9dd38dcaf5fc6eca64effbafe6792156
Tim Squires <tim@crystalmark.co.uk>
2020-06-18 15:37:15 +0100

    python to update cron

--- capture.sh
+++ capture.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "0 * * * *	~/boatcam/capture.sh" | crontab -
+bucket=boatcamtest
 cd ~/boatcam
 [ ! -f test ] && git checkout .
 [ ! -f test ] && git pull 2>&1
@@ -7,12 +7,12 @@ rm -rf __pycache__
 sudo service gpsd restart
 sleep 2
 i=0
-until python3 capture.py boatcam 2>&1 ; do
+until python3 capture.py $bucket 2>&1 ; do
   ((i = i + 1))
   [[ i -ge 10 ]] && echo "Failed!" && break
   sleep 30
   [ ! -f test ] && git pull 2>&1
   [ ! -f test ] && rm -rf __pycache__
 done
-[ ! -f test ] && aws s3 cp /var/mail/pi "s3://$1/pi_mail"
+[ ! -f test ] && aws s3 cp /var/mail/pi "s3://$bucket/pi_mail"
 [ ! -f test ] && cat /dev/null > /var/mail/pi
