7dcca3914013fc67999785ca62de8d4dbb311c9d
Tim Squires <tim@crystalmark.co.uk>
2020-07-02 19:47:08 +0100

    Change to voltage read by current monitor pHat

--- Voltage.py
+++ Voltage.py
@@ -1,25 +1,22 @@
 import board
-import busio
-import adafruit_ads1x15.ads1115 as ADS
-from adafruit_ads1x15.analog_in import AnalogIn
-import sys
-from Gyro import Gyro
-from Uploader import Uploader
-from State import State
-from Position import Position
-from BoatImage import BoatImage
-
-VOLTAGE_RATIO_ENGINE = 4.9
-VOLTAGE_RATIO_LEISURE = 5.8
+from adafruit_ina219 import ADCResolution, BusVoltageRange, INA219
 
 
 class Voltages:
     def __init__(self):
         try:
-            i2c = busio.I2C(board.SCL, board.SDA)
-            ads = ADS.ADS1115(i2c)
-            self.chan1 = AnalogIn(ads, ADS.P0)
-            self.chan2 = AnalogIn(ads, ADS.P1)
+            i2c_bus = board.I2C()
+
+            self.chan1 = INA219(i2c_bus, addr=0x40)
+            self.chan2 = INA219(i2c_bus, addr=0x41)
+
+            self.chan1.bus_adc_resolution = ADCResolution.ADCRES_12BIT_32S
+            self.chan1.shunt_adc_resolution = ADCResolution.ADCRES_12BIT_32S
+            self.chan1.bus_voltage_range = BusVoltageRange.RANGE_16V
+
+            self.chan2.bus_adc_resolution = ADCResolution.ADCRES_12BIT_32S
+            self.chan2.shunt_adc_resolution = ADCResolution.ADCRES_12BIT_32S
+            self.chan2.bus_voltage_range = BusVoltageRange.RANGE_16V
         except:
             print("No voltage monitor connected")
             self.chan1 = None
@@ -28,8 +25,8 @@ class Voltages:
     def read_voltages(self):
 
         if self.chan1 is not None:
-            voltage1 = round((self.chan1.voltage * VOLTAGE_RATIO_LEISURE), 2)
-            voltage2 = round((self.chan2.voltage * VOLTAGE_RATIO_ENGINE), 2)
+            voltage1 = round(self.chan1.shunt_voltage, 2)
+            voltage1 = round(self.chan2.shunt_voltage, 2)
         else:
             voltage1 = None
             voltage2 = None
