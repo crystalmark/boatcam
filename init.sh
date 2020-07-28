8fa5808d88c0db1e15156f84f67c821bcf3fb840
Tim Squires <tim@crystalmark.co.uk>
2020-07-28 11:51:12 +0100

    Removed keyboard input of api key

--- init.sh
+++ init.sh
@@ -6,8 +6,8 @@ serialnumber="$(cut -d'-' -f5 <<<"$uid")"
 echo $serialnumber >> ~pi/.serialnumber
 echo "Serial Number: $serialnumber"
 
-echo "Please enter the new api key for $serialnumber:"
-read API_KEY
+API_KEY=$1
+echo "Using $API_KEY:"
 echo $API_KEY >> ~pi/.apikey
 
 curl -s --header "Content-Type: application/json" --header "x-api-key: $API_KEY"  --request POST  https://whqprggu22.execute-api.eu-west-2.amazonaws.com/beta/boatcam/$serialnumber
